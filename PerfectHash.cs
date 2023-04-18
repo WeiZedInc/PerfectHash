8557c235237a391a817d5d4b5062a15c2df7309c
Yurii <50027889+WeiZedInc@users.noreply.github.com>
2023-04-18 14:00:30 +0300

    Initial commit

---
+++ PerfectHash.cs
@@ -0,0 +1,69 @@
+﻿using System.Numerics;
+
+namespace PH
+{
+
+    public class MyHashTable
+    {
+        //змінні
+        int _size;
+        List<Complex>[] _table;
+        Random _rnd;
+        const int MaxPrimeSize = 100;
+        readonly int RandomValue;
+
+        int GetHash(Complex key) => (int)((key.Magnitude * RandomValue) % PrimeNum(_size)) % _size; // генерація хешу
+
+
+        public MyHashTable(int size) // конструктор класу, иніціалізація змінних
+        {
+            _size = size;
+            _table = new List<Complex>[size];
+            for (int i = 0; i < size; i++)
+                _table[i] = new List<Complex>();
+            _rnd = new Random();
+            RandomValue = _rnd.Next(1, 200);
+        }
+
+        public void Add(Complex key) // створення хешу, та додання в таблицю
+        {
+            int hash = GetHash(key); // hash
+            if (!_table[hash].Contains(key))
+                _table[hash].Add(key); // key
+            else
+                Console.WriteLine("Знайдено коллiзiю(дублiкат) для iндексу: " + hash + ", значення: " + key + ". Ключ не додається до таблицi!");
+        }
+
+
+        int PrimeNum(int tableSize) // пошук простого числа для вибірки
+        {
+            int count = 0, res = 0;
+            for (int i = tableSize + 1; i < MaxPrimeSize; i++)
+            {
+                for (int j = 1; j < MaxPrimeSize; j++)
+                    if (i % j == 0) count++;
+
+                if (count < 3)
+                {
+                    res = i;
+                    return res;
+                }
+                count = 0;
+            }
+            return res;
+        }
+
+        public void Print() // вивід таблиці у консоль
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                Console.Write("[" + i + "]: ");
+                foreach (Complex key in _table[i])
+                    Console.Write(key + " ");
+
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Numerics;
+
+namespace PH
+{
+    internal class Program
+    {
+        //Ініціалізація змінних
+        static int size = 15;
+        static MyHashTable myTable = new MyHashTable(size);
+        static Complex[] keys = new Complex[size];
+        static Random rnd = new Random();
+
+        static void Main(string[] args) => OutputResult(); //точка входу в програму
+
+        static void OutputResult() // обрахунки та вивід у консоль
+        {
+            //Заповнення змінних
+            var real = rnd.Next(1, 214);
+            var imaginary = rnd.NextDouble();
+            Complex key;
+            for (int i = 0; i < size; i++)
+            {
+                key = new Complex(real, imaginary); // псевдо-випадкове комплексне число
+                if (keys.Contains(key)) // перевірка на дублікат
+                {
+                    size++;
+                    Console.WriteLine("Знайдено дублікат початкового ключа. (Пропускаемо, та генеруємо інший)");
+                    continue;
+                }
+                keys[i] = key; // додаємо дійсне число до вибірки
+                myTable.Add(key);  // обрахунок
+            }
+
+            //Вивід у консоль
+            PrintToConsole(myTable, keys);
+        }
+
+        static void PrintToConsole(MyHashTable myTable, Complex[] keys) // метод для виводу у консоль
+        {
+            myTable.Print();
+            Console.WriteLine("\nПочатковi значення:");
+            for (int i = 0; i < keys.Length; i++)
+                Console.WriteLine($"{i}. " + keys[i] + " ");
+
+            Console.WriteLine();
+        }
+    }
+}
